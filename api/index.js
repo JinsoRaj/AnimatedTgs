0b42b72da0d5d51c9063ef42767e222b79f144d9
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2021-11-28 15:46:20 +0530

    ..

--- api/index.js
+++ api/index.js
@@ -27,12 +27,12 @@ app.get('/text2svg', async(req, res) => {
     const svgTag = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512">`
 
     const defsvgPath = TextToSVG.loadSync().getPath(`${text}`, options);
-    const btbsvgPath = TextToSVG.loadSync('./public/fonts/BacktoBlack.ttf').getPath(`${text}`, options);
-    const bcsvgPath  = TextToSVG.loadSync('./public/fonts/Blkchcry.ttf').getPath(`${text}`, options);
-    const cfsvgPath  = TextToSVG.loadSync('./public/fonts/CafeFrancoise.otf').getPath(`${text}`, options);
-    const crsvgPath  = TextToSVG.loadSync('./public/fonts/CookieRegular.ttf').getPath(`${text}`, options);
-    const qhsvgPath  = TextToSVG.loadSync('./public/fonts/Quikhand.ttf').getPath(`${text}`, options);
-    const ymsvgPath  = TextToSVG.loadSync('./public/fonts/YouMurdererBB.ttf').getPath(`${text}`, options);
+    const btbsvgPath = TextToSVG.loadSync('./api/fonts/BacktoBlack.ttf').getPath(`${text}`, options);
+    const bcsvgPath  = TextToSVG.loadSync('./api/fonts/Blkchcry.ttf').getPath(`${text}`, options);
+    const cfsvgPath  = TextToSVG.loadSync('./api/fonts/CafeFrancoise.otf').getPath(`${text}`, options);
+    const crsvgPath  = TextToSVG.loadSync('./api/fonts/CookieRegular.ttf').getPath(`${text}`, options);
+    const qhsvgPath  = TextToSVG.loadSync('./api/fonts/Quikhand.ttf').getPath(`${text}`, options);
+    const ymsvgPath  = TextToSVG.loadSync('./api/fonts/YouMurdererBB.ttf').getPath(`${text}`, options);
 
     res.json({
         text: `${text}`,
